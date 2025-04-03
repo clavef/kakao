b312a6bf8cc759a5f128c3ce512b02ce877758c9
clavef <id@matt.kr>
2025-04-03 14:32:35 +0900

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+// pages/index.tsx
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>kakao.games</title>
+        <meta name="description" content="작고 엉뚱한 웹게임 컬렉션" />
+      </Head>
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+        <h1 className="text-4xl font-bold mt-10">🎮 kakao.games</h1>
+        <p className="mt-4 text-lg text-gray-600">
+          작고 엉뚱한 웹게임들을 만날 수 있는 공간<br />
+          by Studio J & The Madmade
+        </p>
+        <div className="mt-6 space-x-4">
+          <a href="/games" className="px-6 py-2 bg-black text-white rounded-xl">게임 플레이하기</a>
+          <a href="/about" className="px-6 py-2 border border-black rounded-xl">소개 보기</a>
+        </div>
+      </main>
+    </>
+  );
+}
